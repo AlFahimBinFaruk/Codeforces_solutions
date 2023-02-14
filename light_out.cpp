7188566a94a2bd76dd0836723ecc619b99f9a2e5
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-02-14 20:57:00 +0600

    Create light_out.cpp

---
+++ light_out.cpp
@@ -0,0 +1,79 @@
+// https://codeforces.com/problemset/problem/275/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int givenArr[3][3];
+    cin >> givenArr[0][0] >> givenArr[0][1] >> givenArr[0][2];
+    cin >> givenArr[1][0] >> givenArr[1][1] >> givenArr[1][2];
+    cin >> givenArr[2][0] >> givenArr[2][1] >> givenArr[2][2];
+
+    int result[3][3] = {
+        {1, 1, 1},
+        {1, 1, 1},
+        {1, 1, 1}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            int t = givenArr[i][j];
+
+            for (int a = 0; a < t; a++)
+            {
+                if (result[i][j] == 0)
+                {
+                    result[i][j] = 1;
+                }
+                else if (result[i][j] == 1)
+                {
+                    result[i][j] = 0;
+                }
+
+                if (result[i - 1][j] == 0 && ((i - 1) >= 0))
+                {
+                    result[i - 1][j] = 1;
+                }
+                else if (result[i - 1][j] == 1 && ((i - 1) >= 0))
+                {
+                    result[i - 1][j] = 0;
+                }
+
+                if (result[i + 1][j] == 0 && ((i + 1) <= 2))
+                {
+                    result[i + 1][j] = 1;
+                }
+                else if (result[i + 1][j] == 1 && ((i + 1) <= 2))
+                {
+                    result[i + 1][j] = 0;
+                }
+
+                if (result[i][j - 1] == 0 && ((j - 1) >= 0))
+                {
+                    result[i][j - 1] = 1;
+                }
+                else if (result[i][j - 1] == 1 && ((j - 1) >= 0))
+                {
+                    result[i][j - 1] = 0;
+                }
+
+                if (result[i][j + 1] == 0 && ((j + 1) <= 2))
+                {
+                    result[i][j + 1] = 1;
+                }
+                else if (result[i][j + 1] == 1 && ((j + 1) <= 2))
+                {
+                    result[i][j + 1] = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << result[i][0] << result[i][1] << result[i][2] << endl;
+    }
+
+    return 0;
+}
