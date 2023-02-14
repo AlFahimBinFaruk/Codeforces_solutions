b705bec33fff5d9599639e01da336693980e865e
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-02-14 19:15:24 +0600

    Create queue_at_school.cpp

---
+++ queue_at_school.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/266/B
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n,t;
+    string s;
+
+    cin >> n >> t;
+    cin >> s;
+
+    while(t--){
+        for(int i=1; i<n; i++){
+            if(s[i]=='G' && s[i-1] == 'B'){
+                s[i] = 'B';
+                s[i-1] = 'G';
+                i++;
+            }
+        }
+    }
+
+    cout << s << endl;
+    return 0;
+}
