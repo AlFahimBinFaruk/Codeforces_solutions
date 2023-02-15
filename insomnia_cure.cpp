b09dfef651840b235d13febf052855f8ef242643
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-02-15 20:17:25 +0600

    Create insomnia_cure.cpp

---
+++ insomnia_cure.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/148/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+
+    if (k == 1 || l == 1 || m == 1 || n == 1)
+    {
+        cout << d << endl;
+        return 0;
+    }
+
+    int count = d;
+
+    for (int i = 1; i <= d; i++)
+    {
+        if (i % k != 0 && i % l != 0 && i % m != 0 && i % n != 0)
+        {
+            count--;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
